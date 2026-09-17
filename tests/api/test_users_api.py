bf55606612d8aa494d57f8186825b141fbe2f100
Nedim Tunovic <tunovicnedim@gmail.com>
2026-09-17 20:47:20 +0200

    Improve API response validations

--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -6,16 +6,30 @@ def test_get_user(api_client):
     response = api_client.get("/users/2")
 
     assert response.status_code == 200
+    assert "application/json" in response.headers["Content-Type"]
 
     body = response.json()
 
+    expected_fields = {
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "avatar",
+    }
+
+    assert expected_fields.issubset(body["data"].keys())
+
     assert body["data"]["id"] == 2
+    assert isinstance(body["data"]["id"], int)
+    assert isinstance(body["data"]["email"], str)
     assert body["data"]["email"]
     
 def test_get_non_existing_user(api_client):
     response = api_client.get("/users/23")
 
     assert response.status_code == 404
+    assert "application/json" in response.headers["Content-Type"]
     assert response.json() == {}
     
 def test_create_user(api_client):
@@ -27,13 +41,27 @@ def test_create_user(api_client):
     response = api_client.post("/users", payload)
 
     assert response.status_code == 201
+    assert "application/json" in response.headers["Content-Type"]
 
     body = response.json()
 
-    assert body["name"] == "Nedim"
-    assert body["job"] == "QA Engineer"
+    expected_fields = {
+        "name",
+        "job",
+        "id",
+        "createdAt",
+    }
+
+    assert expected_fields.issubset(body.keys())
+
+    assert body["name"] == payload["name"]
+    assert body["job"] == payload["job"]
+
+    assert isinstance(body["id"], str)
     assert body["id"]
-    assert body["createdAt"]    
+
+    assert isinstance(body["createdAt"], str)
+    assert body["createdAt"]
     
 def test_update_user(api_client):
     payload = {
@@ -44,11 +72,22 @@ def test_update_user(api_client):
     response = api_client.put("/users/2", payload)
 
     assert response.status_code == 200
+    assert "application/json" in response.headers["Content-Type"]
 
     body = response.json()
 
+    expected_fields = {
+        "name",
+        "job",
+        "updatedAt",
+    }
+
+    assert expected_fields.issubset(body.keys())
+
     assert body["name"] == payload["name"]
     assert body["job"] == payload["job"]
+
+    assert isinstance(body["updatedAt"], str)
     assert body["updatedAt"]
     
 def test_delete_user(api_client):
